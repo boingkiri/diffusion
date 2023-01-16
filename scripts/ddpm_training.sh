03c69283e21938e1a681d89c6b04edb9a075815c
boingkiri <djfelrl11@kaist.ac.kr>
2023-01-16 06:31:31 +0000

    modify config path

---
+++ scripts/ddpm_training.sh
@@ -0,0 +1,12 @@
+# nohup python3 run_cifar10.py &
+# tail -f nohup.out
+
+nohup python3 main.py \
+    --config configs/ddpm.yml \
+    --do_train \
+    --do_sampling \
+    --model ddpm \
+    --num_sampling 50000 \
+    &
+tail -f nohup.out
+
