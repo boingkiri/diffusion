aa2788292862134a29e0788403d69e498182e49f
boingkiri <djfelrl11@kaist.ac.kr>
2023-01-26 05:09:38 +0000

    Move vector quantizer to module file

--- model/autoencoder.py
+++ model/autoencoder.py
@@ -3,7 +3,7 @@ import jax
 import jax.numpy as jnp
 
 from typing import Union, Tuple, List 
-from model.modules import UnetUp, UnetMiddle, UnetDown, Downsample, Upsample
+from model.modules import UnetUp, UnetMiddle, UnetDown, Downsample, Upsample, VectorQuantizer
 from framework.autoencoder.distribution import DiagonalGaussianDistribution
 
 
@@ -91,52 +91,6 @@ class Decoder(nn.Module):
         x = self.conv_out(x)
 
         return x
-
-
-class VectorQuantizer(nn.Module):
-    """
-    Original github: https://github.com/CompVis/taming-transformers/blob/3ba01b241669f5ade541ce990f7650a3b8f65318/taming/modules/vqvae/quantize.py#L213
-    This model is ported from pytorch to jax, flax.
-
-    """
-    # NOTE: due to a bug the beta term was applied to the wrong term. for
-    # backwards compatibility we use the buggy version by default, but you can
-    # specify legacy=False to fix it.
-    n_e: int
-    e_dim: int
-    beta: float
-    remap: str = None
-    unknown_index: str = "random"
-    sane_index_shape: bool = False
-    legacy: bool = True
-
-    def setup(self):
-        def zero_centered_uniform(key, shape, dtype=jnp.float_):
-            scale = 1.0 / self.n_e
-            data = jax.random.uniform(key, shape, minval=-scale, maxval=scale)
-            return data
-        self.embedding = nn.Embed(self.n_e, self.e_dim, embedding_init=zero_centered_uniform)
-
-    def __call__(self, z):
-        z_flatten = jnp.reshape(z, (-1, self.e_dim))
-        # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
-        embedding_value = self.embedding.embedding
-        d = jnp.sum(z_flatten ** 2, axis=1, keepdims=True) + \
-            jnp.sum(embedding_value ** 2, axis=1) - \
-            2 * jnp.einsum('bd, dn -> bn', z_flatten, jnp.transpose(embedding_value))
-        min_encoding_indices = jnp.argmin(d, axis=1)
-        z_q = self.embedding(min_encoding_indices)
-        z_q = jnp.reshape(z_q, z.shape)
-        perplexity = None
-        min_encodings = None
-
-        loss = self.beta * jnp.mean((jax.lax.stop_gradient(z_q) - z) ** 2) + \
-                jnp.mean((z_q - jax.lax.stop_gradient(z)) ** 2)
-        
-        # Preserve gradients
-        z_q = z + jax.lax.stop_gradient(z_q - z)
-
-        return z_q, loss, (perplexity, min_encodings, min_encoding_indices)
 
 
 class AbstractAutoEncoder(nn.Module):
