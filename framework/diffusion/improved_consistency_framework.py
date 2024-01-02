Fixed LPIPS loss input scaling

framework/diffusion/improved_consistency_framework.py
@@ -195,8 +195,8 @@ class iCMFramework(DefaultModel):
             output_shape = (y.shape[0], 224, 224, y.shape[-1])
             D_x = jax.image.resize(D_x, output_shape, "bilinear")
             prev_D_x = jax.image.resize(prev_D_x, output_shape, "bilinear")
-            D_x = (D_x + 1) / 2.0
-            prev_D_x = (prev_D_x + 1) / 2.0
+            # D_x = (D_x + 1) / 2.0
+            # prev_D_x = (prev_D_x + 1) / 2.0
             lpips_dist = jnp.mean(self.perceptual_loss(D_x, prev_D_x))
 
             # Monitor difference between original CM and trained (perturbed) CM