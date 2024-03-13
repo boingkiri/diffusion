066d1f750051e293fa02df0d0f4eb8fa7dbeba4a
boingkiri <djfelrl11@kaist.ac.kr>
2024-03-13 07:20:18 +0000

    Enable jax array option

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def start(config: DictConfig):
         # Assume that the running environment is TPU
         # If there is a need to use GPU, the code should be modified: TODO
         jax.distributed.initialize()
-        jax.config.update('jax_array', False)
+        # jax.config.update('jax_array', False)
 
     print("-------------------Config Setting---------------------")
     print(OmegaConf.to_yaml(config))
