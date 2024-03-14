df005b9fa4df7b047d5941e9c12cd1e34f315452
boingkiri <djfelrl11@kaist.ac.kr>
2024-03-14 03:29:29 +0000

    tmp

--- framework/diffusion/edm_framework.py
+++ framework/diffusion/edm_framework.py
@@ -182,11 +182,11 @@ class EDMFramework(DefaultModel):
         return jax_utils.create_train_state(config, 'diffusion', self.model.apply, params)
 
     def get_model_state(self):
-        # return [flax.jax_utils.unreplicate(self.model_state)]
-        if self.distributed_training:
-            return [jax_utils.fully_replicated_host_local_array_to_global_array(self.model_state)]
-        else:
-            return [flax.jax_utils.unreplicate(self.model_state)]
+        return [flax.jax_utils.unreplicate(self.model_state)]
+        # if self.distributed_training:
+        #     return [jax_utils.fully_replicated_host_local_array_to_global_array(self.model_state)]
+        # else:
+        #     return [flax.jax_utils.unreplicate(self.model_state)]
     
     def fit(self, x0, cond=None, step=0):
         key, dropout_key = jax.random.split(self.rand_key, 2)
