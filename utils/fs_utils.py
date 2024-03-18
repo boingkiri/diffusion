977f7392ad7113de38bb2b1db532449441208901
boingkiri <djfelrl11@kaist.ac.kr>
2024-03-18 10:39:23 +0000

    Remove debug mode and fix minor detail

--- utils/fs_utils.py
+++ utils/fs_utils.py
@@ -231,11 +231,10 @@ class FSUtils():
                 global_mesh = jax.sharding.Mesh(jax.devices(), 'model')
                 pspecs = jax.sharding.PartitionSpec('model')
                 try:
-                    x = jax.experimental.multihost_utils.host_local_array_to_global_array(x, global_mesh, pspecs)
+                    x = jax.experimental.multihost_utils.host_local_array_to_global_array(x, global_mesh, pspecs)[0]
                 except:
                     print(f"Error in converting {path} to global array.")
                     ValueError(f"Error in converting {path} to global array.")
-                print(x.shape)
                 return x
             # states = jax.tree_map(
             #     lambda x: orbax.checkpoint.utils.fully_replicated_host_local_array_to_global_array(x), states)
