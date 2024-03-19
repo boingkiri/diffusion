999ee808295c2d8828ca33b1ba5150ffd7e55921
boingkiri <djfelrl11@kaist.ac.kr>
2024-03-19 12:11:05 +0000

    remove replicate

--- framework/diffusion/edm_framework.py
+++ framework/diffusion/edm_framework.py
@@ -55,8 +55,8 @@ class EDMFramework(DefaultModel):
         #     self.model_state = jax.experimental.multihost_utils.broadcast_one_to_all(self.model_state)
         #     self.model_state = {model_key: jax.experimental.multihost_utils.broadcast_one_to_all(self.model_state[model_key])
         #                         for model_key in self.model_state.keys()}
-        self.model_state = {model_key: flax.jax_utils.replicate(self.model_state[model_key]) 
-                            for model_key in self.model_state.keys()}
+        # self.model_state = {model_key: flax.jax_utils.replicate(self.model_state[model_key]) 
+        #                     for model_key in self.model_state.keys()}
         if self.distributed_training:
             # self.model_state = {model_key: jax.experimental.multihost_utils.broadcast_one_to_all(self.model_state[model_key])
             #                     for model_key in self.model_state.keys()}
