b50210934e7b5e3d40d93ff6d2608e074e192b29
boingkiri <djfelrl11@kaist.ac.kr>
2024-03-20 15:56:01 +0000

    tmp

--- utils/fs_utils.py
+++ utils/fs_utils.py
@@ -270,22 +270,34 @@ class FSUtils():
     def load_model_state(self, state):
         print(f"Get into the load_model_state")
         step = self.checkpoint_manager.latest_step()
-        if step is not None:
-            abstract_train_state = jax.tree_util.tree_map(
-                orbax.checkpoint.utils.to_shape_dtype_struct, state
+        # if step is not None:
+        #     abstract_train_state = jax.tree_util.tree_map(
+        #         orbax.checkpoint.utils.to_shape_dtype_struct, state
+        #     )
+        #     state = self.checkpoint_manager.restore(
+        #         step, 
+        #         args=orbax.checkpoint.args.Composite(
+        #             **{
+        #                 k: orbax.checkpoint.args.StandardRestore(v)
+        #                 for k, v in abstract_train_state.items() if v is not None
+        #             }
+        #         )
+        #     )
+        #     print(f"Loading ckpt of Step {step} complete.")
+        # else:
+        #     print("No ckpt loaded. Start from scratch.")
+        abstract_train_state = jax.tree_util.tree_map(
+            orbax.checkpoint.utils.to_shape_dtype_struct, state
+        )
+        state = self.checkpoint_manager.restore(
+            step, 
+            args=orbax.checkpoint.args.Composite(
+                **{
+                    k: orbax.checkpoint.args.StandardRestore(v)
+                    for k, v in abstract_train_state.items() if v is not None
+                }
             )
-            state = self.checkpoint_manager.restore(
-                step, 
-                args=orbax.checkpoint.args.Composite(
-                    **{
-                        k: orbax.checkpoint.args.StandardRestore(v)
-                        for k, v in abstract_train_state.items() if v is not None
-                    }
-                )
-            )
-            print(f"Loading ckpt of Step {step} complete.")
-        else:
-            print("No ckpt loaded. Start from scratch.")
+        )
         # if self.config.get("distributed_training", False):
         #     # state = jax.tree_map(lambda x: jax.experimental.multihost_utils.broadcast_one_to_all(x), state)
         #     state = flax.jax_utils.replicate(state)
