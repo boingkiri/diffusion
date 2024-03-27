099988e7179b44758f5342b6e5146cf8945984b8
boingkiri <djfelrl11@kaist.ac.kr>
2024-03-27 06:17:10 +0000

    add broadcast one to all to check imaginary

--- framework/diffusion/consistency_framework.py
+++ framework/diffusion/consistency_framework.py
@@ -64,6 +64,11 @@ class CMFramework(DefaultModel):
         if self.head_state is not None:
             states["head"] = self.head_state
         states = fs_obj.load_model_state(states)
+
+        if config["distributed_training"]:
+            states = jax.experimental.multihost_utils.broadcast_one_to_all(states)
+            states = jax.tree_map(lambda x: jnp.asarray(x), states)
+
         self.torso_state, self.head_state = states.get("diffusion"), states.get("head")
         # Replicate states for training with pmap
         self.training_states = {}
